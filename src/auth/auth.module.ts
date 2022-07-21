4dd67e2db74f3768c9d299aa867b762956b889f8
Anang20 <anangsyah766@gmail.com>
2022-07-21 15:20:01 +0700

    menambahkan expires token jwt 1 hari

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -15,7 +15,7 @@ import { JwtStrategy } from './jwt.strategy';
     JwtModule.register({
       secret: 'topSecret20',
       signOptions: {
-        expiresIn: 180,
+        expiresIn: 86400,
       },
     }),
     TypeOrmModule.forFeature([User]),
