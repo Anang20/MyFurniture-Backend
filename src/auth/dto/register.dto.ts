de5f926871aabae4e86f01a804724641a110c71a
Anang20 <anangsyah766@gmail.com>
2022-07-21 10:31:23 +0700

    menambahkan register dto

---
+++ src/auth/dto/register.dto.ts
@@ -0,0 +1,25 @@
+import { IsEmail, IsNotEmpty, IsString, Matches, MaxLength, MinLength } from 'class-validator';
+
+export class RegisterDto {
+    @IsNotEmpty()
+    @IsString()
+    @MinLength(6)
+    @MaxLength(30)
+    nama_lengkap: string;
+
+    @IsNotEmpty()
+    @IsEmail()
+    email: string;
+
+    @IsNotEmpty()     
+    @MinLength(11)
+    @MaxLength(13)
+    no_telp: number;
+
+    @IsNotEmpty()
+    @IsString()
+    @MinLength(8)
+    @MaxLength(30)
+    @Matches(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^*-]).*$/)
+    password: string;
+}
