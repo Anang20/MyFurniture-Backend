d2a161dfc1ca89fec8fa7d3bf3b105bc806f5e45
Anang20 <anangsyah766@gmail.com>
2022-08-07 13:14:23 +0700

    fix: order

--- src/order/order.module.ts
+++ src/order/order.module.ts
@@ -4,9 +4,14 @@ import { OrderController } from './order.controller';
 import { managementOngkir } from './entities/management-ongkir.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Order } from './entities/order.entity';
+import { UsersModule } from 'src/users/users.module';
+import { Alamat } from 'src/users/entities/alamat.entity';
+import { User } from 'src/users/entities/user.entity';
+import { Cart } from 'src/cart/entities/cart.entity';
+import { cartDetail } from 'src/cart/entities/cart-detail.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Order, managementOngkir])],
+  imports: [TypeOrmModule.forFeature([Order, managementOngkir, Alamat, User, Cart, cartDetail]), UsersModule],
   providers: [OrderService],
   controllers: [OrderController]
 })
