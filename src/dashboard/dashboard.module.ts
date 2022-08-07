420ac672923ea4a33d00c25025cbc4e86ee1c503
mardini1414 <fernandaiqshal111@gmail.com>
2022-08-07 14:56:53 +0700

    FIX : dashboard

--- src/dashboard/dashboard.module.ts
+++ src/dashboard/dashboard.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { DashboardService } from './dashboard.service';
 import { DashboardController } from './dashboard.controller';
+import { Payment } from 'src/payment/entities/payment.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'src/users/entities/user.entity';
+import { Produk } from 'src/produk/entities/produk.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Payment, Produk, User])],
   providers: [DashboardService],
   controllers: [DashboardController]
 })
