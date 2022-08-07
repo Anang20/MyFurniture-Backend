9c9f567a012143689c45be1e1587d41b60c5e5d8
mardini1414 <fernandaiqshal111@gmail.com>
2022-08-07 14:49:36 +0700

    FEAT : Menambahkan Dashboard

--- src/dashboard/dashboard.controller.ts
+++ src/dashboard/dashboard.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, HttpStatus } from '@nestjs/common';
+import { DashboardService } from './dashboard.service';
 
 @Controller('dashboard')
-export class DashboardController {}
+export class DashboardController {
+    constructor(
+        private readonly dashboardService: DashboardService
+    ){}
+
+    @Get()
+    async findDashboard(){
+        return {
+            data: await this.dashboardService.findDashboard(),
+            statusCode: HttpStatus.CREATED,
+            massage : 'success'
+        }
+    }
+
+}
--- src/dashboard/dashboard.service.ts
+++ src/dashboard/dashboard.service.ts
@@ -1,4 +1,41 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { hasSubscribers } from 'diagnostics_channel';
+import { Payment } from 'src/payment/entities/payment.entity';
+import { Produk } from 'src/produk/entities/produk.entity';
+import { User } from 'src/users/entities/user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class DashboardService {}
+export class DashboardService {
+    constructor(
+        @InjectRepository(User)
+        private userRepository: Repository<User>,
+        @InjectRepository(Produk)
+        private produkRepository: Repository<Produk>,
+        @InjectRepository(Payment)
+        private paymentRepository: Repository<Payment>
+    ){}
+
+    async findDashboard(){
+        const user = await this.userRepository.findAndCount().then((data)=>{
+            const [user, number] = data
+            return number
+        })
+        const produk = await this.produkRepository.findAndCount().then((data)=>{
+            const [user, number] = data
+            return number
+        })
+        const payment = await this.paymentRepository.findAndCount().then((data)=>{
+            const [user, number] = data
+            return number
+        })
+        const hasil = {
+            user: user,
+            produk: produk,
+            payment: payment
+        }
+        return hasil
+    }
+
+}
