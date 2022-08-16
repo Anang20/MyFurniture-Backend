0d75375bed350ef6778f9ba7806c200044c4b23d
mardini1414 <fernandaiqshal111@gmail.com>
2022-08-16 19:43:46 +0700

    fix:order

--- src/order/order.controller.ts
+++ src/order/order.controller.ts
@@ -1,87 +1,109 @@
-import { Body, Controller, Get, HttpStatus, Param, ParseUUIDPipe, Post, Put } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpStatus,
+  Param,
+  ParseUUIDPipe,
+  Post,
+  Put,
+  Res,
+} from '@nestjs/common';
 import { UpdateOngkirDto } from './dto/updateOngkir.dto';
 import { CreateOrderDto } from './dto/createOrder.dto';
 import { OrderService } from './order.service';
 
 @Controller('order')
 export class OrderController {
-    constructor(
-        private readonly orderService : OrderService
-    ){}
+  constructor(private readonly orderService: OrderService) {}
 
-    @Post('buatOngkir')
-    async updateOngkir(@Body() updateOngkirDto : UpdateOngkirDto){
-        return {
-            data: await this.orderService.updateOngkir(updateOngkirDto),
-            statusCode: HttpStatus.OK,
-            massage : 'success'
-        }
-    }
+  @Post('buatOngkir')
+  async updateOngkir(@Body() updateOngkirDto: UpdateOngkirDto) {
+    return {
+      data: await this.orderService.updateOngkir(updateOngkirDto),
+      statusCode: HttpStatus.OK,
+      massage: 'success',
+    };
+  }
 
-    @Post('ongkir/:id_alamat_user')
-    async createOngkirTotal(
-        @Param('id_alamat_user', ParseUUIDPipe) id_alamat_user:string,
-    ){
-        return {
-            data: await this.orderService.createOngkirTotal(id_alamat_user),
-            statusCode: HttpStatus.CREATED,
-            massage : 'success'
-        }
-    }
+  @Post('ongkir/:id_alamat_user')
+  async createOngkirTotal(
+    @Param('id_alamat_user', ParseUUIDPipe) id_alamat_user: string,
+  ) {
+    return {
+      data: await this.orderService.createOngkirTotal(id_alamat_user),
+      statusCode: HttpStatus.CREATED,
+      massage: 'success',
+    };
+  }
 
-    @Get('/total_hrg_produk/:id_cart')
-    async totalHargaProduk(@Param('id_cart', ParseUUIDPipe) id_cart: string){
-        return {
-            data : await this.orderService.totalHargaProduk(id_cart),
-            statusCode: HttpStatus.OK
-        }
-    }
+  @Get('/total_hrg_produk/:id_cart')
+  async totalHargaProduk(@Param('id_cart', ParseUUIDPipe) id_cart: string) {
+    return {
+      data: await this.orderService.totalHargaProduk(id_cart),
+      statusCode: HttpStatus.OK,
+    };
+  }
 
-    @Post('buat/Order')
-    async createOrder(@Body() createOrder : CreateOrderDto){
-        return {
-            data : await this.orderService.createOrder(createOrder),
-            statusCode : HttpStatus.CREATED,
-            massage : "success"
-        }
-    }
-    @Get(':id_user')
-    async findOneOrder(@Param('id_user', ParseUUIDPipe) id_user : string){
-        return await this.orderService.findOneOrder(id_user)
-    }
+  @Post('buat/Order')
+  async createOrder(@Body() createOrder: CreateOrderDto) {
+    return {
+      data: await this.orderService.createOrder(createOrder),
+      statusCode: HttpStatus.CREATED,
+      massage: 'success',
+    };
+  }
+  @Get(':id_user')
+  async findOneOrder(@Param('id_user', ParseUUIDPipe) id_user: string) {
+    return await this.orderService.findOneOrder(id_user);
+  }
 
-    @Get('cart/:id_user')
-    async findOneCart(@Param('id_user', ParseUUIDPipe) id_user:string){
-        return{
-            data: await this.orderService.findOneCart(id_user)
-        }
-    }
+  @Get('cart/:id_user')
+  async findOneCart(@Param('id_user', ParseUUIDPipe) id_user: string) {
+    return {
+      data: await this.orderService.findOneCart(id_user),
+    };
+  }
 
-    @Get()
-    async findAll(){
-        return {
-            data: await this.orderService.findAll()
-        }
-    }
+  @Get()
+  async findAll() {
+    return {
+      data: await this.orderService.findAll(),
+    };
+  }
 
-    @Put('terima/:id_user')
-    async terima(@Param('id_user', ParseUUIDPipe) id_user: string){
-        return {
-            data: await this.orderService.terima(id_user)
-        }
-    }
+  @Put('terima/:id_user')
+  async terima(@Param('id_user', ParseUUIDPipe) id_user: string) {
+    return {
+      data: await this.orderService.terima(id_user),
+    };
+  }
 
-    @Get('excel/generator')
-    async exportExcel(){
-        return this.orderService.exportExcel()
-    }
+  @Get('excel/generator')
+  async exportExcel() {
+    try {
+       return await this.orderService.exportExcel(); 
+    } catch (error) {
+        return 'terdapat kesalahan saat membuat excel'
+    }  
+  }
 
-    @Get('produk/:id_cart')
-    async findCart(
-        @Param('id_cart', ParseUUIDPipe) id_cart:string
-    ){
-        return {
-            data: await this.orderService.findCart(id_cart)
-        }
-    }
+  @Get('export/excel-generator')
+  async downloadExcel(@Res() res) {
+    try {
+        return await res.download(
+      `./uploads/export/${await this.exportExcel()}`,
+      'laporan.xlsx',
+    );
+    } catch (error) {
+        return 'ada kesalahan dalam mendowload excel'
+    }  
+  }
+
+  @Get('produk/:id_cart')
+  async findCart(@Param('id_cart', ParseUUIDPipe) id_cart: string) {
+    return {
+      data: await this.orderService.findCart(id_cart),
+    };
+  }
 }
--- src/order/order.service.ts
+++ src/order/order.service.ts
@@ -12,275 +12,314 @@ import { Order } from './entities/order.entity';
 import { Produk } from 'src/produk/entities/produk.entity';
 import { Role } from 'src/users/entities/role.entity';
 import { generateExcel } from 'src/helper/export_excel';
+import { async } from 'rxjs';
 // import { generateExcel } from 'src/helper/export_excel';
 
 @Injectable()
 export class OrderService {
-    constructor(
-        @InjectRepository(Role)
-        private roleRepository: Repository<Role>,
-        @InjectRepository(managementOngkir)
-        private ongkirRepository : Repository<managementOngkir>,
-        @InjectRepository(Order)
-        private orderRepository : Repository<Order>,
-        @InjectRepository(Alamat)
-        private alamatRepository : Repository<Alamat>,
-        @InjectRepository(Cart)
-        private cartRepository: Repository<Cart>,
-        @InjectRepository(User)
-        private userRepository: Repository<User>,
-    ){}
+  constructor(
+    @InjectRepository(Role)
+    private roleRepository: Repository<Role>,
+    @InjectRepository(managementOngkir)
+    private ongkirRepository: Repository<managementOngkir>,
+    @InjectRepository(Order)
+    private orderRepository: Repository<Order>,
+    @InjectRepository(Alamat)
+    private alamatRepository: Repository<Alamat>,
+    @InjectRepository(Cart)
+    private cartRepository: Repository<Cart>,
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+  ) {}
 
-    // Untuk Admin
-    async updateOngkir( updateOngkirDto : UpdateOngkirDto){
-        const hasil = await this.ongkirRepository.findOneOrFail({
-            where: {
-                id_harga_kirim : 1,
-            }
-        })
-        hasil.harga_kirim = updateOngkirDto.harga_kirim
-        hasil.jarak = updateOngkirDto.jarak
-        await this.ongkirRepository.save(hasil)
-        return await this.ongkirRepository.findOneOrFail({
-            where: {
-                id_harga_kirim : hasil.id_harga_kirim
-            }
-        })
-    }
-    
-    // Untuk Users
+  // Untuk Admin
+  async updateOngkir(updateOngkirDto: UpdateOngkirDto) {
+    const hasil = await this.ongkirRepository.findOneOrFail({
+      where: {
+        id_harga_kirim: 1,
+      },
+    });
+    hasil.harga_kirim = updateOngkirDto.harga_kirim;
+    hasil.jarak = updateOngkirDto.jarak;
+    await this.ongkirRepository.save(hasil);
+    return await this.ongkirRepository.findOneOrFail({
+      where: {
+        id_harga_kirim: hasil.id_harga_kirim,
+      },
+    });
+  }
 
-    async totalHargaProduk(id_cart: string){
-        const Cart = await this.cartRepository.find({
-            relations:{
-                detail: true
-            },
-            where : {
-                id_cart : id_cart,
-                detail:{
-                    status: 'dipilih'
-                }
-            }, 
-        })
-        const detail= []
-        detail[0]= Cart[0].detail
-        const harga: number[] = []
-        detail[0].map(async (i) => {    
-           await harga.push(i.harga_total)
-        })     
-        const totalHarga = harga.reduce((a, b) => {
-            return a + b
-        })
-        return totalHarga
-    }
+  // Untuk Users
 
-    async findCart(id_cart: string){
-        const Cart = await this.cartRepository.find({
-            relations:{
-                detail:true
-            },
-            where : {
-                id_cart : id_cart,
-                detail:{
-                    status: 'dipilih'
-                }
-            }, 
-        })
-        console.log(Cart, 'ini cart');
-        
-        const produk = Cart[0].detail
-        console.log(produk, 'ini produk');
-        
-        return produk
-    }
+  async totalHargaProduk(id_cart: string) {
+    const Cart = await this.cartRepository.find({
+      relations: {
+        detail: true,
+      },
+      where: {
+        id_cart: id_cart,
+        detail: {
+          status: 'dipilih',
+        },
+      },
+    });
+    const detail = [];
+    detail[0] = Cart[0].detail;
+    const harga: number[] = [];
+    detail[0].map(async (i) => {
+      await harga.push(i.harga_total);
+    });
+    const totalHarga = harga.reduce((a, b) => {
+      return a + b;
+    });
+    return totalHarga;
+  }
 
-    async createOrder(createOrder : CreateOrderDto){
-        const alamat = await this.alamatRepository.findOneOrFail({
-            where: {
-                id_alamat_user : createOrder.id_alamat
-            }
-        })
-        const ongkir = await this.ongkirRepository.findOne({
-            where: {
-                id_harga_kirim: 1
-            }
-        })
-        const cart = await this.cartRepository.findOneOrFail({
-            where: {
-                id_cart: createOrder.id_cart
-            }
-        })
-        console.log(cart);
-        
-        const order = new Order()
-        order.alamat = alamat
-        order.cart = cart
-        order.total_hrg_brg = createOrder.total_hrg_brg
-        order.total_hrg_krm = createOrder.total_hrg_krm
-        order.total_order = createOrder.total_order
-        order.ongkir = ongkir
-        order.status = 'belum bayar'
-        await this.orderRepository.insert(order)  
-        return await this.orderRepository.findOneOrFail({
-            where: {
-                id_order : order.id_order
-            }
-        })  
-    }
+  async findCart(id_cart: string) {
+    const Cart = await this.cartRepository.find({
+      relations: {
+        detail: true,
+      },
+      where: {
+        id_cart: id_cart,
+        detail: {
+          status: 'dipilih',
+        },
+      },
+    });
+    console.log(Cart, 'ini cart');
 
-    async createOngkirTotal(id_alamat_user: string){
-        const alamat = await this.alamatRepository.findOneOrFail({
-            where:{
-                id_alamat_user
-            }
-        })
-        const user = await this.roleRepository.findOne({
-            where: {
-                role_name: 'admin'
-            }, relations: {
-                user: {
-                    alamat: true
-                }
-            }
-        })     
-        const alamatAdmin = user.user[0].alamat[0]
-        const latAdmin = alamatAdmin.latitude    
-        const longAdmin = alamatAdmin.longtitude
-        const latUser = alamat.latitude
-        const longUser = alamat.longtitude     
-        const ongkir = await this.ongkirRepository.findOne({
-            where: {
-                id_harga_kirim : 1
-            }
-        })
-        const harga_kirim = ongkir.harga_kirim / ongkir.jarak
-        function distance(lat1, lon1, lat2, lon2, unit) {
-            if ((lat1 == lat2) && (lon1 == lon2)) {
-                return 0;
-            }
-            else {
-                var radlat1 = Math.PI * lat1/180;
-                var radlat2 = Math.PI * lat2/180;
-                var theta = lon1-lon2;
-                var radtheta = Math.PI * theta/180;
-                var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
-                if (dist > 1) {
-                    dist = 1;
-                }
-                dist = Math.acos(dist);
-                dist = dist * 180/Math.PI;
-                dist = dist * 60 * 1.1515;
-                if (unit=="K") { dist = dist * 1.609344 }
-                if (unit=="N") { dist = dist * 0.8684 }
-                const hasil = Math.round(dist) * harga_kirim
-            
-                return hasil;
-            }
+    const produk = Cart[0].detail;
+    console.log(produk, 'ini produk');
+
+    return produk;
+  }
+
+  async createOrder(createOrder: CreateOrderDto) {
+    const alamat = await this.alamatRepository.findOneOrFail({
+      where: {
+        id_alamat_user: createOrder.id_alamat,
+      },
+    });
+    const ongkir = await this.ongkirRepository.findOne({
+      where: {
+        id_harga_kirim: 1,
+      },
+    });
+    const cart = await this.cartRepository.findOneOrFail({
+      where: {
+        id_cart: createOrder.id_cart,
+      },
+    });
+    console.log(cart);
+
+    const order = new Order();
+    order.alamat = alamat;
+    order.cart = cart;
+    order.total_hrg_brg = createOrder.total_hrg_brg;
+    order.total_hrg_krm = createOrder.total_hrg_krm;
+    order.total_order = createOrder.total_order;
+    order.ongkir = ongkir;
+    order.status = 'belum bayar';
+    await this.orderRepository.insert(order);
+    return await this.orderRepository.findOneOrFail({
+      where: {
+        id_order: order.id_order,
+      },
+    });
+  }
+
+  async createOngkirTotal(id_alamat_user: string) {
+    const alamat = await this.alamatRepository.findOneOrFail({
+      where: {
+        id_alamat_user,
+      },
+    });
+    const user = await this.roleRepository.findOne({
+      where: {
+        role_name: 'admin',
+      },
+      relations: {
+        user: {
+          alamat: true,
+        },
+      },
+    });
+    const alamatAdmin = user.user[0].alamat[0];
+    const latAdmin = alamatAdmin.latitude;
+    const longAdmin = alamatAdmin.longtitude;
+    const latUser = alamat.latitude;
+    const longUser = alamat.longtitude;
+    const ongkir = await this.ongkirRepository.findOne({
+      where: {
+        id_harga_kirim: 1,
+      },
+    });
+    const harga_kirim = ongkir.harga_kirim / ongkir.jarak;
+    function distance(lat1, lon1, lat2, lon2, unit) {
+      if (lat1 == lat2 && lon1 == lon2) {
+        return 0;
+      } else {
+        var radlat1 = (Math.PI * lat1) / 180;
+        var radlat2 = (Math.PI * lat2) / 180;
+        var theta = lon1 - lon2;
+        var radtheta = (Math.PI * theta) / 180;
+        var dist =
+          Math.sin(radlat1) * Math.sin(radlat2) +
+          Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+        if (dist > 1) {
+          dist = 1;
         }
-       const hasil =  distance(latAdmin, longAdmin, latUser, longUser, harga_kirim)
-       return hasil
-    }
-
-    async findOneOrder(id_user : string){
-        const user = await this.userRepository.findOneOrFail({
-            where:{
-                id_user : id_user
-            }, relations: {
-                cart: {
-                    order: true
-                }
-            }
-        })
-        const order = user.cart.order
-        return order
-    }
-
-    async findOneCart(id_user: string){
-        const user = await this.userRepository.find({
-            where: {
-                id_user,
-                cart:{
-                    detail: {
-                        status: 'dipilih'
-                    }
-                }
-            }, relations:{
-                cart:{
-                    detail: true
-                }
-            }
-        })
-        console.log(user);
-        
-        return {
-            detail: user[0].cart.detail,
-            id_cart: user[0].cart.id_cart
+        dist = Math.acos(dist);
+        dist = (dist * 180) / Math.PI;
+        dist = dist * 60 * 1.1515;
+        if (unit == 'K') {
+          dist = dist * 1.609344;
         }
-    }
+        if (unit == 'N') {
+          dist = dist * 0.8684;
+        }
+        const hasil = Math.round(dist) * harga_kirim;
 
-    async findAll(){
-        const order = await this.orderRepository.find({
-            where:{
-                status:'sudah bayar'
-            }
-        })
-        console.log(order[0],'ini order');
+        return hasil;
+      }
+    }
+    const hasil = distance(latAdmin, longAdmin, latUser, longUser, harga_kirim);
+    return hasil;
+  }
 
-    }
+  async findOneOrder(id_user: string) {
+    const user = await this.userRepository.findOneOrFail({
+      where: {
+        id_user: id_user,
+      },
+      relations: {
+        cart: {
+          order: true,
+        },
+      },
+    });
+    const order = user.cart.order;
+    return order;
+  }
 
-    async terima(id_order: string){
-        const order = await this.orderRepository.findOneOrFail({
-            where: {
-                id_order: id_order
-            }
-        })
-        order.status = 'telah dikirim'
-        await this.orderRepository.save(order)
-        return this.orderRepository.findOneOrFail({
-            where: {
-                id_order: order.id_order
-            }
-        })
+  async findOneCart(id_user: string) {
+    const user = await this.userRepository.find({
+      where: {
+        id_user,
+        cart: {
+          detail: {
+            status: 'dipilih',
+          },
+        },
+      },
+      relations: {
+        cart: {
+          detail: true,
+        },
+      },
+    });
+    console.log(user);
+
+    return {
+      detail: user[0].cart.detail,
+      id_cart: user[0].cart.id_cart,
+    };
+  }
+
+  async findAll() {
+    const order = await this.orderRepository.find({
+      where: {
+        status: 'sudah bayar',
+      },
+      relations: ['cart.detail.produk','alamat.kelurahan.kecamatan.kota.provinsi',]
+  })
+    console.log(order[0].cart);
+    let array = [];
+    for (let i = 0; i < order.length; i++) {
+      console.log(order[i].alamat);
+      console.log(order[i].alamat.kelurahan, 'kotaaaaaaaaaa');
+
+      const data = await {
+        No: i + 1,
+        Tanggal: order[i].created_at,
+        Nama: order[i].cart.user,
+        Produk: order[i].cart.detail[i].produk.nama_produk,
+        Kuantiti: order[i].cart.detail[i].kuantiti,
+        HargaBarang: order[i].cart.detail[i].produk.harga,
+        totalHarga: order[i].cart.detail[i].harga_total,
+        // Alamat: `${order[i].alamat.kelurahan.kecamatan.kota.provinsi.nama_provinsi}, ${order[i].alamat.kelurahan.kecamatan.nama_kecamatan}, ${order[i].alamat.kelurahan.nama_kelurahan}, ${order[i].alamat.alamat}`,
+      };
+      array.push(data);
     }
-    async exportExcel(){
-        const order = await this.orderRepository.find({
-        relations:{
-                cart: {
-                    detail:{
-                        produk: true
-                    },
-                    user: true
-                }
-            }, where: {
-                status: 'telah dikirim'
-            }
-        })
-        const detail = []
-        // console.log(order[0].cart.detail[0]);
-        
-        detail[0] = order[0].cart.detail
-        const Tanggal = order[0].cart.created_at
-        const Nama = order[0].cart.user.nama_lengkap
-        const harga_kirim = order[0].total_hrg_krm
-        const harga_total = order[0].total_order
-        const data = []
-        detail[0].map(async (i, index) =>{
-            console.log(i, 'ini i');
-            
-            await data.push([
-                Tanggal,
-                Nama,
-                i.produk.nama_produk,
-                i.kuantiti,
-                i.produk.harga,
-                harga_kirim,
-                harga_total,
-            ])
-        })
-        console.log(data, 'Ini data');
-        
-        const excel = await generateExcel(data, 'Hit-Log-Api')
-        console.log(excel, 'ini excel');
-        return excel
+    return array;
+
+    // console.log(data);
+
+    // order.map(async (order[i]) => {
+    // let no = 1 + i
+
+    //   return await data.push({
+    //         // No: no,
+    //         Tanggal: value.created_at,
+    //         Nama: value.cart.user,
+    //         Produk: value.cart.detail.produk.nama_produk,
+    //         Kuantiti: value.cart.detail.kuantiti,
+    //         HargaBarang: value.cart.detail.produk.harga,
+    //         totalHarga: value.cart.detail.harga_total,
+    //         Alamat: `${value.alamat.kelurahan.kecamatan.kota.provinsi.nama_provinsi}, ${value.alamat.kelurahan.kecamatan.nama_kecamatan}, ${value.alamat.kelurahan.nama_kelurahan}, ${value.alamat.alamat}`
+    //   })
+    // })
+    // console.log(data);
+    // return await data
+  }
+
+  async terima(id_order: string) {
+    const order = await this.orderRepository.findOneOrFail({
+      where: {
+        id_order: id_order,
+      },
+    });
+    order.status = 'telah dikirim';
+    await this.orderRepository.save(order);
+    return this.orderRepository.findOneOrFail({
+      where: {
+        id_order: order.id_order,
+      },
+    });
+  }
+  async exportExcel() {
+    try {
+      const order = await this.orderRepository.find({
+        relations: {
+          cart: true,
+        },
+        where: {
+          status: 'telah dikirim',
+        },
+      });
+      const detail = [];
+      detail[0] = order[0].cart.detail;
+      const Tanggal = order[0].cart.created_at;
+      const Nama = order[0].cart.user.nama_lengkap;
+      const harga_kirim = order[0].total_hrg_krm;
+      const harga_total = order[0].total_order;
+      const data = [];
+      detail[0].map(async (i, index) => {
+        await data.push([
+          Tanggal.getDate(),
+          Nama,
+          i.produk.nama_produk,
+          i.kuantiti,
+          i.produk.harga,
+          harga_kirim,
+          harga_total,
+        ]);
+      });
+      const excel = await generateExcel(data, 'Hit-Log-Api');
+      return excel;
+    } catch (e) {
+      return `{e}`;
     }
+  }
 }
