b2674ed6222b3507b15bd63b2b315b16f6ecbbfd
mardini1414 <fernandaiqshal111@gmail.com>
2022-08-30 12:36:40 +0700

    feat:Dashboard

--- src/dashboard/dashboard.controller.ts
+++ src/dashboard/dashboard.controller.ts
@@ -3,24 +3,30 @@ import { DashboardService } from './dashboard.service';
 
 @Controller('dashboard')
 export class DashboardController {
-    constructor(
-        private readonly dashboardService: DashboardService
-    ){}
+  constructor(private readonly dashboardService: DashboardService) {}
 
-    @Get()
-    async findDashboard(){
-        return {
-            data: await this.dashboardService.findDashboard(),
-            statusCode: HttpStatus.CREATED,
-            massage : 'success'
-        }
-    }
+  @Get()
+  async findDashboard() {
+    return {
+      data: await this.dashboardService.findDashboard(),
+      statusCode: HttpStatus.CREATED,
+      massage: 'success',
+    };
+  }
 
-    @Get('user/')
-    async findCustomer(){
-        return {
-            data: await this.dashboardService.findCustomer()
-        }
-    }
+  @Get('user/')
+  async findCustomer() {
+    return {
+      data: await this.dashboardService.findCustomer(),
+    };
+  }
 
+  @Get('data/transaksi/')
+  async getDataTransaksi() {
+    return await this.dashboardService.getDataTransaksi();
+  }
+  @Get('data/registrasi/')
+  async getDataRegister(){
+    return await this.dashboardService.getDataRegistrasi();
+  }
 }
--- src/dashboard/dashboard.service.ts
+++ src/dashboard/dashboard.service.ts
@@ -9,54 +9,283 @@ import { Repository } from 'typeorm';
 
 @Injectable()
 export class DashboardService {
-    constructor(
-        @InjectRepository(Role)
-        private roleRepository: Repository<Role>,
-        @InjectRepository(Produk)
-        private produkRepository: Repository<Produk>,
-        @InjectRepository(Payment)
-        private paymentRepository: Repository<Payment>,
-        @InjectRepository(User)
-        private userRepository: Repository<User>
-    ){}
+  constructor(
+    @InjectRepository(Role)
+    private roleRepository: Repository<Role>,
+    @InjectRepository(Produk)
+    private produkRepository: Repository<Produk>,
+    @InjectRepository(Payment)
+    private paymentRepository: Repository<Payment>,
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+  ) {}
 
-    async findDashboard(){
-        const user = await this.userRepository.findAndCountBy({}).then((data)=>{
-            const [user, number] = data
-            return number - 1
-        })
-        
-        const produk = await this.produkRepository.findAndCount().then((data)=>{
-            const [user, number] = data
-            return number
-        })
-        const payment = await this.paymentRepository.findAndCount({
-            where : {
-                status : 'diterima'
-            }
-        }).then((data)=>{
-            const [user, number] = data
-            return number
-        })
-        const hasil = {
-            user: user,
-            produk: produk,
-            payment: payment
-        }
-        return hasil
-    }
+  async findDashboard() {
+    const user = await this.userRepository.findAndCountBy({}).then((data) => {
+      const [user, number] = data;
+      return number - 1;
+    });
 
-    async findCustomer(){
-        const role = await this.roleRepository.find({
-            relations: {
-                user: true
-            }, where:{
-                role_name: 'customer'
-            }
-        })
-        const user = role[0].user
-        console.log(user);
-        return user
-    }
+    const produk = await this.produkRepository.findAndCount().then((data) => {
+      const [user, number] = data;
+      return number;
+    });
+    const payment = await this.paymentRepository
+      .findAndCount({
+        where: {
+          status: 'diterima',
+        },
+      })
+      .then((data) => {
+        const [user, number] = data;
+        return number;
+      });
+    const hasil = {
+      user: user,
+      produk: produk,
+      payment: payment,
+    };
+    return hasil;
+  }
 
+  async findCustomer() {
+    const role = await this.roleRepository.find({
+      relations: {
+        user: true,
+      },
+      where: {
+        role_name: 'customer',
+      },
+    });
+    const user = role[0].user;
+    console.log(user);
+    return user;
+  }
+
+  async getDataTransaksi() {
+    const payment = await this.paymentRepository.find({
+      where: {
+        status: 'diterima',
+      },
+    });
+    const Januari = [];
+    const Februari = [];
+    const Maret = [];
+    const April = [];
+    const Mei = [];
+    const Juni = [];
+    const Juli = [];
+    const Agustus = [];
+    const September = [];
+    const Oktober = [];
+    const November = [];
+    const Desember = [];
+    const JanuariOld = [];
+    const FebruariOld = [];
+    const MaretOld = [];
+    const AprilOld = [];
+    const MeiOld = [];
+    const JuniOld = [];
+    const JuliOld = [];
+    const AgustusOld = [];
+    const SeptemberOld = [];
+    const OktoberOld = [];
+    const NovemberOld = [];
+    const DesemberOld = [];
+    payment.map((value) => {
+      const month = value.updated_at.getMonth() + 1;
+      const yearNow = new Date();
+      const year = value.updated_at.getFullYear();
+      if (month == 1 && year == yearNow.getFullYear()) {
+        Januari.push(value);
+      } else if (month == 2 && year == yearNow.getFullYear()) {
+        Februari.push(value);
+      } else if (month == 3 && year == yearNow.getFullYear()) {
+        Maret.push(value);
+      } else if (month == 4 && year == yearNow.getFullYear()) {
+        April.push(value);
+      } else if (month == 5 && year == yearNow.getFullYear()) {
+        Mei.push(value);
+      } else if (month == 6 && year == yearNow.getFullYear()) {
+        Juni.push(value);
+      } else if (month == 7 && year == yearNow.getFullYear()) {
+        Juli.push(value);
+      } else if (month == 8 && year == yearNow.getFullYear()) {
+        Agustus.push(value);
+      } else if (month == 9 && year == yearNow.getFullYear()) {
+        September.push(value);
+      } else if (month == 10 && year == yearNow.getFullYear()) {
+        Oktober.push(value);
+      } else if (month == 11 && year == yearNow.getFullYear()) {
+        November.push(value);
+      } else if (month == 12 && year == yearNow.getFullYear()) {
+        Desember.push(value);
+      } else if (month == 1 && year == yearNow.getFullYear() - 1) {
+        JanuariOld.push(value);
+      } else if (month == 2 && year == yearNow.getFullYear() - 1) {
+        FebruariOld.push(value);
+      } else if (month == 3 && year == yearNow.getFullYear() - 1) {
+        MaretOld.push(value);
+      } else if (month == 4 && year == yearNow.getFullYear() - 1) {
+        AprilOld.push(value);
+      } else if (month == 5 && year == yearNow.getFullYear() - 1) {
+        MeiOld.push(value);
+      } else if (month == 6 && year == yearNow.getFullYear() - 1) {
+        JuniOld.push(value);
+      } else if (month == 7 && year == yearNow.getFullYear() - 1) {
+        JuliOld.push(value);
+      } else if (month == 8 && year == yearNow.getFullYear() - 1) {
+        AgustusOld.push(value);
+      } else if (month == 9 && year == yearNow.getFullYear() - 1) {
+        SeptemberOld.push(value);
+      } else if (month == 10 && year == yearNow.getFullYear() - 1) {
+        OktoberOld.push(value);
+      } else if (month == 11 && year == yearNow.getFullYear() - 1) {
+        NovemberOld.push(value);
+      } else if (month == 12 && year == yearNow.getFullYear() - 1) {
+        DesemberOld.push(value);
+      }
+    });
+    return {
+      Now: {
+        Januari: Januari.length,
+        Februari: Februari.length,
+        Maret: Maret.length,
+        April: April.length,
+        Mei: Mei.length,
+        Juni: Juni.length,
+        Juli: Juli.length,
+        Agustus: Agustus.length,
+        September: September.length,
+        Oktober: Oktober.length,
+        November: November.length,
+        Desember: Desember.length,
+      },
+      Old: {
+        JanuariOld: Januari.length,
+        FebruariOld: Februari.length,
+        MaretOld: Maret.length,
+        AprilOld: April.length,
+        MeiOld: Mei.length,
+        JuniOld: Juni.length,
+        JuliOld: Juli.length,
+        AgustusOld: Agustus.length,
+        SeptemberOld: September.length,
+        OktoberOld: Oktober.length,
+        NovemberOld: November.length,
+        DesemberOld: Desember.length,
+      },
+    };
+  }
+  async getDataRegistrasi() {
+    const User = await this.userRepository.find();
+    const Januari = [];
+    const Februari = [];
+    const Maret = [];
+    const April = [];
+    const Mei = [];
+    const Juni = [];
+    const Juli = [];
+    const Agustus = [];
+    const September = [];
+    const Oktober = [];
+    const November = [];
+    const Desember = [];
+    const JanuariOld = [];
+    const FebruariOld = [];
+    const MaretOld = [];
+    const AprilOld = [];
+    const MeiOld = [];
+    const JuniOld = [];
+    const JuliOld = [];
+    const AgustusOld = [];
+    const SeptemberOld = [];
+    const OktoberOld = [];
+    const NovemberOld = [];
+    const DesemberOld = [];
+    User.map((value) => {
+      const month = value.created_at.getMonth() + 1;
+      const yearNow = new Date();
+      const year = value.updated_at.getFullYear();
+      if (month == 1 && year == yearNow.getFullYear()) {
+        Januari.push(value);
+      } else if (month == 2 && year == yearNow.getFullYear()) {
+        Februari.push(value);
+      } else if (month == 3 && year == yearNow.getFullYear()) {
+        Maret.push(value);
+      } else if (month == 4 && year == yearNow.getFullYear()) {
+        April.push(value);
+      } else if (month == 5 && year == yearNow.getFullYear()) {
+        Mei.push(value);
+      } else if (month == 6 && year == yearNow.getFullYear()) {
+        Juni.push(value);
+      } else if (month == 7 && year == yearNow.getFullYear()) {
+        Juli.push(value);
+      } else if (month == 8 && year == yearNow.getFullYear()) {
+        Agustus.push(value);
+      } else if (month == 9 && year == yearNow.getFullYear()) {
+        September.push(value);
+      } else if (month == 10 && year == yearNow.getFullYear()) {
+        Oktober.push(value);
+      } else if (month == 11 && year == yearNow.getFullYear()) {
+        November.push(value);
+      } else if (month == 12 && year == yearNow.getFullYear()) {
+        Desember.push(value);
+      } else if (month == 1 && year == yearNow.getFullYear() - 1) {
+        JanuariOld.push(value);
+      } else if (month == 2 && year == yearNow.getFullYear() - 1) {
+        FebruariOld.push(value);
+      } else if (month == 3 && year == yearNow.getFullYear() - 1) {
+        MaretOld.push(value);
+      } else if (month == 4 && year == yearNow.getFullYear() - 1) {
+        AprilOld.push(value);
+      } else if (month == 5 && year == yearNow.getFullYear() - 1) {
+        MeiOld.push(value);
+      } else if (month == 6 && year == yearNow.getFullYear() - 1) {
+        JuniOld.push(value);
+      } else if (month == 7 && year == yearNow.getFullYear() - 1) {
+        JuliOld.push(value);
+      } else if (month == 8 && year == yearNow.getFullYear() - 1) {
+        AgustusOld.push(value);
+      } else if (month == 9 && year == yearNow.getFullYear() - 1) {
+        SeptemberOld.push(value);
+      } else if (month == 10 && year == yearNow.getFullYear() - 1) {
+        OktoberOld.push(value);
+      } else if (month == 11 && year == yearNow.getFullYear() - 1) {
+        NovemberOld.push(value);
+      } else if (month == 12 && year == yearNow.getFullYear() - 1) {
+        DesemberOld.push(value);
+      }
+    });
+    return {
+      Now: {
+        Januari: Januari.length,
+        Februari: Februari.length,
+        Maret: Maret.length,
+        April: April.length,
+        Mei: Mei.length,
+        Juni: Juni.length,
+        Juli: Juli.length,
+        Agustus: Agustus.length,
+        September: September.length,
+        Oktober: Oktober.length,
+        November: November.length,
+        Desember: Desember.length,
+      },
+      Old: {
+        JanuariOld: Januari.length,
+        FebruariOld: Februari.length,
+        MaretOld: Maret.length,
+        AprilOld: April.length,
+        MeiOld: Mei.length,
+        JuniOld: Juni.length,
+        JuliOld: Juli.length,
+        AgustusOld: Agustus.length,
+        SeptemberOld: September.length,
+        OktoberOld: Oktober.length,
+        NovemberOld: November.length,
+        DesemberOld: Desember.length,
+      },
+    };
+  }
 }
